c8c34b3ae57e2d259e509423012ea96008d63cdf
april <doingusmcscrumply@gmail.com>
2026-04-23 08:38:15 +0200

    added saveand load functionality
    basically all the base requirements met need to add dialougs for stuff (chekcing with a yes no if user wants to save) and i need to fix the title to not be a complete filepath and then the other stuff like drag and drop ect ect.. theres an issue with permissions, the program is dissaloowed from reading files outside of its bounds which is fair but annoying in this case, and lets see thers some otehr sstuff too. i wanna add an easter egg but no spoilers abt that just yet ;3

--- vt2026-a3/Form1.cs
+++ vt2026-a3/Form1.cs
@@ -9,54 +9,70 @@ namespace vt2026_a3
         public Form1()
         {
             InitializeComponent();
-            SM = new(txtfeildTxb);
+            SM = SaveManager.getInstance(txtfeildTxb);
             this.Text = SM.Filename;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            titleasterisk();
+            try
+            {
+                SM.Titleasterisk(this);
+            }
+            catch (Exception) { Debug.WriteLine("cannot access 'titleasterisk' from sm (savemanager)"); }
         }
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
         {
             switch (keyData)
             {
-                case Keys.S | Keys.Control: return true;
+                case Keys.S | Keys.Control: saveTsmi.PerformClick(); return true;
+                case Keys.W | Keys.Control: saveAsTsmi.PerformClick(); return true;
+                case Keys.L | Keys.Control: loadTsmi.PerformClick(); return true;
+                case Keys.N | Keys.Control: newTsmi.PerformClick(); return true;
             }
             return base.ProcessCmdKey(ref msg, keyData);
-        }
-        private void titleasterisk()
-        {
-            switch (SM.Altered, txtfeildTxb.Text == SM.InitialText)
-            {
-                case (true, true): SM.Altered = false; this.Text = SM.Filename; break;
-                case (false, false): SM.Altered = true; this.Text = "*" + SM.Filename; break;
-            }
         }
 
         private void exitTsmi_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (SM.Altered) { SM.SaveAs(); }
+            }
+            catch (Exception) { Debug.WriteLine("cannot access property 'altered' from sm (savemanager)"); }
             this.Close();
         }
 
         private void saveTsmi_Click(object sender, EventArgs e)
         {
-
+            try{
+                SM.Save();
+                SM.Titleasterisk(this);
+            }catch(Exception) { Debug.WriteLine("cannot access savemanger (save, titleasterisk)"); }
         }
 
         private void saveAsTsmi_Click(object sender, EventArgs e)
         {
-
+            try{
+                SM.SaveAs();
+                SM.Titleasterisk(this);
+            }catch (Exception) { Debug.WriteLine("cannot access savemanger (saveas, titleasterisk)"); }
         }
 
         private void loadTsmi_Click(object sender, EventArgs e)
         {
-
+            try{
+                SM.Load();
+                SM.Titleasterisk(this);
+            }catch (Exception) { Debug.WriteLine("cannot access savemanger (load, titleasterisk)"); }
         }
 
         private void newTsmi_Click(object sender, EventArgs e)
         {
-
+            try{
+                SM.New();
+                SM.Titleasterisk(this);
+            }catch (Exception) { Debug.WriteLine("cannot access savemanger (new, titleasterisk)"); }
         }
     }
 }
--- vt2026-a3/SaveManager.cs
+++ vt2026-a3/SaveManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,51 +9,123 @@ namespace vt2026_a3
 {
     internal class SaveManager
     {
-        /**
-         * i want to try and implament the singleton pattern in this save manager sine i only rly want one fliereader active at a time
-         * otherwise itll cause issues with the buffer
-         * 
-         * and also its a nice challange sincce i havent ever used it before and seems like
-         * itd be a fun challange
-         * 
-         * if i remember correctly it had a private static constructor and a private static
-         * datatype of whatever u wanna keep 
-         * 
-         *and then also an internal or public method that calls on the preivate constructor
-         *and when using this u cant even use the new keyword
-         *
-         *i feel thisd be neat :3
-         *
-         */
-        private TextBox inputfeild;
-
-        internal string InitialText { get; set; }
-        internal string Filename { get; }
-        internal bool Altered { get; set; }
-        /*/
-        //singleton expiriment
-        private static object thing;
-        private SaveManager() {
-        //something 
-        }
-        internal SaveManager method()
-        {
-            return thing ?? SaveManager();
-        }
-        //singelton expiriment 
-        //**/
-        internal SaveManager(TextBox inputfeild)
-        {
+        //singelton ↓↓↓↓
+        private static SaveManager? instance;
+        private SaveManager(TextBox inputfeild) {
             this.inputfeild = inputfeild;
-
-            InitialText = inputfeild.Text;
-            Filename = "coolest-ever-document.txt";
-            Altered = false;
+            this.filename = null;
+            this.altered = false;
         }
+        static internal SaveManager getInstance(TextBox inputfeild) { return instance ?? (instance = new(inputfeild)); }
+        //singelton ↑↑↑↑
 
+        private const string defaultname = "newfile.txt";
+        private TextBox inputfeild;
+        private string? initialText;
+        private string? filename;
+        private bool altered;
+        internal string? InitialText { get {return initialText; }}
+        internal string? Filename { get{return filename??defaultname; }}
+        internal bool Altered { get{return altered; }}
+
+        /// <summary>
+        /// pre: true
+        /// post: saves and stuff 
+        /// (pretty short just look at the code, using words will take longer to understand)
+        /// </summary>
         internal void Save()
         {
-            InitialText = inputfeild.Text;
+            if (filename == null) { SaveAs(); return; }
+            if (!altered) {return; }
+            using (StreamWriter writer=new(filename))
+            {
+                writer.Write(inputfeild.Text);
+            }
+            initialText = inputfeild.Text;
+            altered = false;
+        }
+        /// <summary>
+        /// pre: permission by os to write files
+        /// post: allows user to save files with specified location and name 
+        /// currently an issue with the name including the file path, will work on it later
+        /// </summary>
+        internal void SaveAs()
+        {
+            SaveFileDialog saveFileDialog = new()
+            {
+                InitialDirectory=Application.StartupPath,
+                //InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments),
+                Filter = "txt files (*.txt)|*.txt",
+                AddToRecent = true,
+                FileName = filename ?? defaultname
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK) 
+            {
+                try
+                {//try catch for safety and unknown risks abt file streaming, how this all is done is inspired by a previous project i have worked on
+                    using (StreamWriter writer = new(saveFileDialog.FileName))
+                    {
+                        writer.Write(initialText);
+                    }
+                    initialText = inputfeild.Text;
+                    altered=false;
+                    filename = saveFileDialog.FileName;
+                }
+                catch (Exception) { }
+            }
+        }
+        /// <summary>
+        /// pre: permission by os to read files
+        /// post: loads a textfile if one is selected by user
+        /// </summary>
+        internal void Load()
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+
+                InitialDirectory = Application.StartupPath,
+                //InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments),
+                Filter = "txt files (*.txt)|*.txt",
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {//try catch just in case idk file reading can be quirky and im not too familiar
+                    using (StreamReader reader = new(openFileDialog.FileName))
+                    {
+                        inputfeild.Text = reader.ReadToEnd();//self explantory
+                    }
+                    initialText = inputfeild.Text;
+                    altered=false;
+                    filename = openFileDialog.FileName;
+                }
+                catch (Exception) { }
+            }
+        }
+        /// <summary>
+        /// pre: true
+        /// post: makes a new yk thingy blank page
+        /// </summary>
+        internal void New()
+        {
+            if (altered) { SaveAs(); }
+            inputfeild.Text = "";
+            initialText = inputfeild.Text;
+            filename = null;
+            altered = false;
+        }
+        /// <summary>
+        /// pre: title!=null
+        /// post: title and altered updated, asterisk added if changed since last save, removed if not 
+        /// </summary>
+        /// <param name="title"></param>
+        internal void Titleasterisk(Form frm)
+        {
+            switch (altered, inputfeild.Text == initialText)
+            {
+                case (true, true): altered = false; frm.Text = filename??defaultname; break;
+                case (false, false): altered = true; frm.Text = "*" + filename; break;
+            }
         }
     }
 }
